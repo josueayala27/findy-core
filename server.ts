a857f0e31bf54e85428ecf20cac2f43ddd55b581
memo124 <48780097+memo124@users.noreply.github.com>
2026-07-04 20:21:37 -0600

    Cambio de nuevo endpoint

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { authRoute } from "./src/routes/auth.route";
 import { meRoute } from "./src/routes/me.route";
 import { placesRoute } from "./src/routes/places.route";
@@ -6,6 +7,16 @@ import { clickedPlacesRoute } from "./src/routes/clicked-places.route";
 import { errorHandler } from "./src/middleware/error-handler";
 
 const app = new Hono();
+
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);
 
 app.onError(errorHandler);
 
